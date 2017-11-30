1f9ec1f421d35d4c6b142ecfb7861e3fd1198ff0
Devil8899 <lzznbachina@163.com>
2017-11-30 18:59:27 +0800

    python

--- Day3/hello.py
+++ Day3/hello.py
@@ -5,7 +5,7 @@ print("tom")
 name = "jerry"
 x = 3 
 y = 4 
-
+print("________定义变量_________________________")
 z = x * y  #赋值给变量
 
 print("x乘以y=", z  ) 
@@ -16,7 +16,23 @@ z = 5*8
 print("z=", z  ) 
 
 # print = 3    不能使用系统关键字给变量赋值
-# print(print)
+# print(print) 打印会报错
+
+print("____________定义常量_______________________")
+Age=20  
+print("常量使用大写定义:",Age)
+
+
+print("________________________________________")
+
+name2=name;
+print("name2:",name2)
+name="jack"
+print("name:",name,"name2",name2)  #name值会变 但name2不会变
+
+#内存回收机制 python是自动回收的.
+
+#手动删除变量 关键字+变量名   del Age  
 
 
 
