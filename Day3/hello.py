9bcb0366e19f3b5a4a56938aada46539ecd6d89b
Devil8899 <lzznbachina@163.com>
2017-11-30 18:27:46 +0800

    python

---
+++ Day3/hello.py
@@ -0,0 +1,23 @@
+print("Hello World!")
+print("tom")
+
+
+name = "jerry"
+x = 3 
+y = 4 
+
+z = x * y  #赋值给变量
+
+print("x乘以y=", z  ) 
+
+print("z=", z  ) 
+
+z = 5*8 
+print("z=", z  ) 
+
+# print = 3    不能使用系统关键字给变量赋值
+# print(print)
+
+
+
+
